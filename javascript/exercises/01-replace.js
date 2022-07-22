16c3b5021f61dd7c26c1b5656aa677e8deb199f3
Stuart Rimel <stuart.rimel@gmail.com>
2022-07-22 10:01:44 -0700

    exercise 1

--- javascript/exercises/01-replace.js
+++ javascript/exercises/01-replace.js
@@ -1,6 +1,9 @@
 const replaceItems = (input) => {
   // return a string with 'gold' replaced by 'SHINY' and 'wander' replaced by 'roam', regardless of the case of the letters
-
+  const rgx_gold = /\b(g|G)(o|O)(l|L)(d|D)\b/;
+  const rgx_wander = /\b(w|W)(a|A)(n|N)(d|D)(e|E)(r|R)\b/;
+  input = input.replace(rgx_gold, 'SHINY');
+  input = input.replace(rgx_wander, 'roam');
   // for reference, see the MDN "Regular Expressions" entry:
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
 
