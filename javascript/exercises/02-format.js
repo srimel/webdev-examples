9af10cded54f54c51c41c97a0ec8698c5b21b9b8
Stuart Rimel <stuart.rimel@gmail.com>
2022-07-25 19:32:11 -0700

    updated exercise 2

--- javascript/exercises/02-format.js
+++ javascript/exercises/02-format.js
@@ -1,8 +1,7 @@
 const formatItems = (input) => {
   // return a string of the concatenated numbers, separated by commas and with three digits after the decimal point
-  const floated = input.map((element) => element.toFixed(3));
-  // console.log(floated);
-  input = floated.toString();
+  let floated = input.map((element) => element.toFixed(3));
+  input = floated.join(', ');
 
   // for reference, see the MDN Number page:
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
